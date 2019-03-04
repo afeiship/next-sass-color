48d5565a7ddaa847627ffae540e8f198c999e29c
afeiship <1290657123@qq.com>
2019-03-04 23:17:59 +0800

    add: test scss

--- test/test.js
+++ test/test.js
@@ -1,17 +1,33 @@
-(function () {
+(function() {
+  var fs = require('fs');
   var nx = require('next-js-core2');
+  var sass = require('node-sass');
   var NxSassColor = require('../src/next-sass-color');
+  var sassString = require('fs')
+    .readFileSync('./test/app.scss')
+    .toString();
 
-  describe('NxSassColor.methods', function () {
-    test('init', function () {
-      var data = {
-        key: 1,
-        value: 2
-      };
-      // console.log(rs1, rs2);
+  // console.log(sassString);
+  // return;
 
-      // expect(rs1).toBe(true);
+  describe('NxSassColor.methods', function() {
+    test('syncRender', function() {
+      var result = sass.renderSync({
+        data: sassString,
+        options: {
+          outputStyle: 'expanded'
+        },
+        functions: NxSassColor.create({
+          primary: {
+            default: '#4cd964',
+            dark: '#f60',
+            light: '#eee'
+          },
+          secondary: '#999'
+        })
+      });
 
+      console.log(result.css.toString());
     });
   });
-}());
+})();
